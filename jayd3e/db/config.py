521c609a03d7a08b92f2483430b0c2d11bf5bca7
Joe Dallago <jd.dallago@gmail.com>
2011-06-21 21:25:36 -0500

    Added a script to backup the database.

--- jayd3e/db/config.py
+++ jayd3e/db/config.py
@@ -1,7 +1,7 @@
 class DbConfig(object):
     engine = 'mysql+mysqldb'
-    user = 'jayd3e'
-    pw = 'sharp7&7'
+    user = ''
+    pw = ''
     host = 'localhost'
     db = 'jayd3e_db'
 
---
+++ jayd3e/db/scripts/backup_database.sh
@@ -0,0 +1,1 @@
+mysqldump -u username -p"pass" jayd3e_db > ../backups/jayd3e_db-$(date +%m-%d-%y_%T).sql
